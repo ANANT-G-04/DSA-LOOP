4adeddada9d0f947c29abd1c3fad8f8f99eb6d3b
ANANT-G-04 <anantgupta455543@gmail.com>
2026-08-14 15:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (65.27%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int n;
+        unsigned int p=0;
+        int k=x;
+      if(x>=0){
+        while(k>0){
+         n=k%10;
+         p=p*10+n;
+        k=k/10;
+        }
+        if(p==x){
+            return true;
+        }
+      }
+      return false;
+    }
+};
