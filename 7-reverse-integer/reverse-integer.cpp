a37f7ab93f8c0af23689ce17b010918ca368a26a
ANANT GUPTA <anantgupta455543@gmail.com>
2026-08-22 13:51:14 +0530

    Time: 3 ms (29.56%) | Memory: 8.4 MB (98.22%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+       
+            int ans=0;
+            int c=0;
+            while(x!=0){
+                c=x%10;
+                if(ans>INT_MAX/10 || ans<INT_MIN/10){
+                    return 0;
+                }
+                ans=ans*10+c;
+                x=x/10;
+            }
+            return ans;
+    }
+};
