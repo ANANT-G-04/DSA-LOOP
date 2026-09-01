6dc169cc8b0dbafe335d50d90db2404f8d3ca5a1
ANANT GUPTA <anantgupta455543@gmail.com>
2026-09-01 12:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.51%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+        long long ans=1;
+        while(ans<n){
+         ans=ans*2;
+        }
+        if(ans==n){
+            return true;
+        }
+        return false;
+    }
+};
