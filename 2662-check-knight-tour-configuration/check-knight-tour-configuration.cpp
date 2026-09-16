c26971d335bc3610f90607e0005ab6bd0aafe268
ANANT GUPTA <anantgupta455543@gmail.com>
2026-09-16 14:23:10 +0530

    Time: 4 ms (5.09%) | Memory: 31.8 MB (36.88%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-bool isValid(vector<vector<int>>& grid,int r,int c,int expVal){
-    if(r>=grid.size() || c>=grid.size() || c<0 || r<0 ||grid[r][c]!=expVal){
+bool isValid(vector<vector<int>>& grid,int r,int c,int expVal,int n){
+    if(r>=n || c>=n || c<0 || r<0 ||grid[r][c]!=expVal){
         return false;
     }
-    if(expVal==grid.size()*grid.size()-1){
+    if(expVal==n*n-1){
         return true;
     }
-    bool ans1=isValid(grid,r-2,c+1,expVal+1);
-    bool ans2=isValid(grid,r-1,c+2,expVal+1);
-    bool ans3=isValid(grid,r+1,c+2,expVal+1);
-    bool ans4=isValid(grid,r+2,c+1,expVal+1);
-    bool ans5=isValid(grid,r+2,c-1,expVal+1);
-    bool ans6=isValid(grid,r+1,c-2,expVal+1);
-    bool ans7=isValid(grid,r-1,c-2,expVal+1);
-    bool ans8=isValid(grid,r-2,c-1,expVal+1);
+    bool ans1=isValid(grid,r-2,c+1,expVal+1,n);
+    bool ans2=isValid(grid,r-1,c+2,expVal+1,n);
+    bool ans3=isValid(grid,r+1,c+2,expVal+1,n);
+    bool ans4=isValid(grid,r+2,c+1,expVal+1,n);
+    bool ans5=isValid(grid,r+2,c-1,expVal+1,n);
+    bool ans6=isValid(grid,r+1,c-2,expVal+1,n);
+    bool ans7=isValid(grid,r-1,c-2,expVal+1,n);
+    bool ans8=isValid(grid,r-2,c-1,expVal+1,n);
     return ans1||ans2||ans3||ans4||ans5||ans6||ans7||ans8;
 }
     bool checkValidGrid(vector<vector<int>>& grid) {
-        return isValid(grid,0,0,0);
+        return isValid(grid,0,0,0,grid.size());
     }
 };
